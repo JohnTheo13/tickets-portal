34873d2a098954b70978de636db70936d4f072ad
Ioannis Theodosiou <ioannis@info.nl>
2020-12-18 17:39:34 +0100

    home screen buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,40 @@
 <template>
-<router-link to="/create" >+</router-link>
-<router-link to="/tickets" >List</router-link>
+  <div class="container">
+    <router-link to="/create"
+      ><Button><div class="icon icon_plus"/></Button
+    ></router-link>
+    <router-link to="/tickets"
+      ><Button><div class="icon icon_list"/></Button
+    ></router-link>
+    <router-link to="/"
+      ><Button disabled><div class="icon icon_profile"/></Button
+    ></router-link>
+  </div>
 </template>
 
 <script>
 // @flow
+import { Button } from '../components'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    Button
+  }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scopped>
+.container {
+  display: flex;
+  margin: spacer(80) auto;
+  position: relative;
+  justify-content: space-evenly;
+  @media (min-width: 768px) {
+    max-width: 60%;
+    button {
+      padding: spacer(18);
+    }
+  }
+}
 </style>
