3b15a254beb7d1cdb3af4df23434587127aac90f
Ioannis Theodosiou <ioannis@info.nl>
2020-12-21 11:58:40 +0100

    buttons alignment in overview

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="container">
-    <router-link to="/create"
-      ><Button><div class="icon icon_plus"/></Button
-    ></router-link>
-    <router-link to="/tickets"
-      ><Button><div class="icon icon_list"/></Button
-    ></router-link>
-    <router-link to="/"
-      ><Button disabled><div class="icon icon_profile"/></Button
-    ></router-link>
+    <router-link to="/create">
+      <Button>
+        <div class="icon icon_plus" />
+        <div>{{ this.t('home_new') }}</div>
+      </Button>
+    </router-link>
+    <router-link to="/tickets">
+      <Button>
+        <div class="icon icon_list" />
+        <div>{{ this.t('home_tickets') }}</div>
+      </Button>
+    </router-link>
+    <router-link to="/">
+      <Button disabled>
+        <div class="icon icon_profile" />
+        <div>{{ this.t('home_profile') }}</div>
+      </Button>
+    </router-link>
   </div>
 </template>
 
@@ -27,14 +36,28 @@ export default {
 <style lang="scss" scopped>
 .container {
   display: flex;
-  margin: spacer(80) auto;
   position: relative;
   justify-content: space-evenly;
-  @media (min-width: 768px) {
-    max-width: 60%;
-    button {
-      padding: spacer(18);
+  flex-direction: column;
+  align-items: center;
+  min-height: spacer(275);
+  button {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: spacer(18);
+    width: spacer(75);
+    height: spacer(75);
+    > div {
+      &:last-child {
+        margin-top: spacer(6);
+      }
     }
+  }
+  @media (min-width: 768px) {
+    margin: spacer(80) auto;
+    flex-direction: row;
+    max-width: 60%;
   }
 }
 </style>
